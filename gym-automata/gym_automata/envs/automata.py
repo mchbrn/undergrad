7bb2d40f97f51027026e4b45fa6e95669a1ab6c6
Michael Burns <mjburns@techknowledg.ie>
2021-02-06 20:47:34 +0000

    Began class definitions

--- gym-automata/gym_automata/envs/automata.py
+++ gym-automata/gym_automata/envs/automata.py
@@ -20,3 +20,37 @@ class Automata(gym.Env):
     def close(self):
         pass
 
+class Automaton:
+    def __init__(self, number, name, x, y, edges, hosts):
+        self.number = number
+        self.name = name
+        self.x = x
+        self.y = y
+        self.edges = edges
+        self.hosts = hosts
+
+    def lock():
+        pass
+    
+    def unlock():
+        pass
+
+class Host:
+    def __init__(self, age, sex, ethnicity, health, activity, state, vertex, cell):
+        self.age = age
+        self.sex = sex
+        self.ethnicity = ethnicity
+        self.health = health
+        self.activity = activity
+        self.state = state
+        self.vertex = vertex
+        self.cell = cell
+
+    def move():
+        pass
+
+    def changeState():
+        pass
+
+    def infect():
+        pass
